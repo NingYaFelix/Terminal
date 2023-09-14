e672b02e6242a94da3d3f7773037c780318413f5
YaningLuo <2272970374@qq.com>
2023-09-14 18:52:46 +0800

    自动化安装

--- Linux/oh-my-zsh/install-main.sh
+++ Linux/oh-my-zsh/install-main.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-# 下载install自动化脚本
-wget --no-cookie --no-check-certificate https://github.com/NingYaFelix/Terminal/blob/main/Linux/oh-my-zsh/install.sh ${HOME}/install.sh
+# 拉取整个库到用户目录
+git clone http://github.com/NingYaFelix/Terminal.git ${HOME}/zsh
+
+#整理拉取的目录
+mv /root/zsh/Linux/oh-my-zsh ${HOME}/oh-my-zsh
+rm -rf ${HOME}/zsh
+cd ${HOME}/zsh
+rm -rf install-main.sh
 
 # 添加权限
 chmod +x install.sh
+./install.sh
 
 # 下载自动补全和高亮插件
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# 下载修改过后的zshrc文件
-wget --no-cookie --no-check-certificate https://github.com/NingYaFelix/Terminal/blob/main/Linux/oh-my-zsh/.zshrc ${HOME}/.zshrc
+#移动zshrc
+mv .zshrc ${HOME}/
 
 # 重启配置
 source .zshrc
